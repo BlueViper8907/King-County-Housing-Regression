be63ea40a01919f170569332d40fc7d0bd39e3a6
Fennec Nightingale <fenneccharles@gmail.com>
2020-12-06 11:55:56 -0800

    working on cleaning new data

--- .virtual_documents/Incorporating_New_Data.ipynb.py
+++ .virtual_documents/Incorporating_New_Data.ipynb.py
@@ -1,15 +1,17 @@
 import pandas as pd 
 import numpy as np
+import os
 
 
 kc_dtypes = {'id': str, 'date' : str,  'price': float, 'bedrooms' : int, 'bathrooms' : float, 'sqft_living': int, 'sqft_lot': int, 'floors': float, 
              'waterfront': float, 'view' : float, 'condition': float, 'grade': int, 'sqft_above': int, 'yr_built': int,
              'yr_renovated': float, 'zipcode': float, 'lat': float, 'long': float, 'sqft_living15': int, 'sqft_lot15': int}
 
-kc_data = pd.read_csv('dsc-phase-2-project/data/kc_house_data.csv', parse_dates = ['date'])
+
+kc_data = pd.read_csv(r '~\Documents\Flatiron\data\kc_house_data.csv', parse_dates = ['date'])
 
 
-kc_parcels = pd.read_csv('dsc-phase-2-project/data/EXTR_Parcel.csv', encoding='ISO-8859-1')
+kc_parcels = pd.read_csv('../data/EXTR_Parcel.csv', encoding='ISO-8859-1')
 
 i = 0
 j = 0
