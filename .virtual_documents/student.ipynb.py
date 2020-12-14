2de646a71a147db7360f5197a8043b78890b51da
Fennec Nightingale <fenneccharles@gmail.com>
2020-12-14 00:01:54 -0800

    added data from food and grocery stores, added a function to plot our coefficients, fixed up final model & train testing, working on cleaning up and adding docstrings and making it easy to walk through

---
+++ .virtual_documents/student.ipynb.py
@@ -0,0 +1,15 @@
+# Your code here - remember to use markdown cells for comments as well!
+
+
+
+#get_ipython().getoutput("pip install import_ipynb")
+import import_ipynb
+
+
+import Exploring_Data as exp
+
+
+exp.
+
+
+
